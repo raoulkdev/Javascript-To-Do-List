d13038c6296e070d77d984f9f6dd2dce497a4b0d
raoul <raouldev@outlook.com>
2025-01-23 17:54:29 -0600

    Task Functionality
    Added creating tasks to main.js and connecting DOM inputs, added base elements to index.html.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,40 @@
+let allTasks = [];
+const nameInput = document.querySelector('.taskName');
+const urgencyInput = document.querySelector('.taskUrgencyDropdown');
+const dueDateInput = document.querySelector('.taskDueDate');
+const subtasksInput = document.querySelector('.subtasks');
+const createBtn = document.querySelector('.createButton');
+
 class Task
 {
   constructor(taskName, urgencyLevel, dueDate, subTasks) {
     this.taskName = taskName;
-    this.urgencyLevel  = ['Low', 'Medium', 'High'];
-    this.dueDate = new Date();
-    this.subTasks = [];
+    this.urgencyLevel  = 'Low';
+    this.dueDate = dueDate
+    this.subTasks = subTasks;
   }
 }
+
+function clearInputs()
+{
+  nameInput.value = '';
+  urgencyInput.value = '';
+  dueDateInput.value = '';
+  subtasksInput.value = '';
+}
+
+createBtn.addEventListener('click', function()
+{
+    let taskName = nameInput.value;
+    let urgencyLevel = urgencyInput.value;
+    let dueDate = dueDateInput.value;
+    let subTasks = subtasksInput.value.split(',' || ', ').map(item => item.trim());
+
+    let newTask = new Task(taskName, urgencyLevel, dueDate, subTasks);
+    allTasks.push(newTask);
+
+    console.log(allTasks);
+    clearInputs();
+  
+});
+
