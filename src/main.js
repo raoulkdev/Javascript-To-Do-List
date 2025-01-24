5d20af127fc3a85cc0034a651c379a2864b4135b
raoul <raouldev@outlook.com>
2025-01-23 18:22:57 -0600

    Finished Adding/Removing Tasks
    Finished Task Container, creating task UI elements in main.js and added deleting functionality

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ const urgencyInput = document.querySelector('.taskUrgencyDropdown');
 const dueDateInput = document.querySelector('.taskDueDate');
 const subtasksInput = document.querySelector('.subtasks');
 const createBtn = document.querySelector('.createButton');
+const taskList = document.querySelector('.taskList');
 
 class Task
 {
@@ -33,6 +34,38 @@ createBtn.addEventListener('click', function()
     let newTask = new Task(taskName, urgencyLevel, dueDate, subTasks);
     allTasks.push(newTask);
 
+    let div = document.createElement('div');
+    div.className = 'taskItem';
+    taskList.appendChild(div);
+
+    let taskTitle = document.createElement('h2');
+    taskTitle.textContent = `${taskName}`;
+    div.appendChild(taskTitle);
+
+    let taskUrgencyText = document.createElement('h2');
+    taskUrgencyText.textContent = `Urgency: ${urgencyLevel}`;
+    div.appendChild(taskUrgencyText);
+
+    let dueDateText = document.createElement('h2');
+    dueDateText.textContent = `Due: ${dueDate}`;
+    div.appendChild(dueDateText);
+
+    let subtasksList = document.createElement('ul');
+    subTasks.forEach(subtask => {
+      let subtaskItem = document.createElement('li');
+      subtaskItem.textContent = subtask;
+      subtasksList.appendChild(subtaskItem);
+    });
+    div.appendChild(subtasksList);
+
+    let deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.addEventListener('click', function()
+    {
+      taskList.removeChild(div);
+      allTasks = allTasks.filter(task => task.taskName!== taskName);
+    });
+    div.appendChild(deleteBtn);
     console.log(allTasks);
     clearInputs();
   
